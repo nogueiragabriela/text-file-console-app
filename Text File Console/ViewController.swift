02eb62be91d71e00f4be8a8063c92f81c8937a90
nogueiragabriela <nogueiragabriela93@gmail.com>
2025-03-28 13:06:06 -0300

    feat: update feature to load and print text file content

--- Text File Console/ViewController.swift
+++ Text File Console/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    
+    @IBAction func loadFileAndPrintButtonPressed(_ sender: UIButton) {
+        
+        if let file = Bundle.main.url(forResource: "example", withExtension: "txt") {
+            do {
+                let content = try String(contentsOf: file, encoding: .utf8)
+                print("File Content:\n\(content)")
+                
+                self.showMessage("✅ File loaded successfully! Check the console to read the text.", color: .systemGreen)
+                
+            } catch {
+                print("Error loading file: \(error.localizedDescription)")
+                self.showMessage("❌ An error occurred while trying to load file! Check the console for more details.", color: .systemRed)
+            }
+        } else {
+            print("File not found in bundle.")
+            self.showMessage("⚠️ File not found in bundle!", color: .systemOrange)
+        }
     }
 
-
+    
+    
+    func showMessage(_ text: String, color: UIColor) {
+        messageLabel.text = text
+        messageLabel.textColor = color
+        messageLabel.alpha = 0
+        UIView.animate(withDuration: 0.5) {
+            self.messageLabel.alpha = 1
+        }
+    }
 }
 
